0872f468baf83b4f09590c5baa5a14f353686a7c
Santu <santujana.1998@gmail.com>
2025-09-17 18:32:14 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <h1>Hello world</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
